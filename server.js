df634cc9a5917dd0e7432b37ec0d74cdbbb2103f
Shikubro90 <shikubro90@gmail.com>
2023-01-19 13:33:19 +0600

    upload

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express")
+const app = express()
+const helmet = require("helmet")
+const multer = require("multer")
+
+app.use(helmet())
+app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded({extended : false}))
+
+
+const storage = multer.diskStorage({
+    destination : (req, file, callback)=>{
+        console.log("+++++++++"+file)
+        callback(null ,"./uploads")
+    },
+    filename : (req, file, callback)=>{
+        console.log(">>>>>>>>>"+file)
+        callback(null, file.originalname)
+    }
+})
+
+const upload = multer({storage}).single("myFiles")
+app.post('/', (req, res)=>{
+    upload(req,res,(error)=>{
+        if(error){
+            res.send("File Upload Failed")
+        }else{
+            res.send("File Upload Success")
+        }
+    })
+})
+
+app.listen(8000)
+console.log("Server Running")
